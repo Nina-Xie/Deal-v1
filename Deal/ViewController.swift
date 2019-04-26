ddd53fc867f568d4c154bc4a7f1af5f8409f9d4d
Nina Xie <nina.xiejingyi@gmail.com>
2019-04-25 23:42:22 -0400

    Initial Commit

---
+++ Deal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deal
+//
+//  Created by 谢静怡 on 4/25/19.
+//  Copyright © 2019 Nina Xie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
